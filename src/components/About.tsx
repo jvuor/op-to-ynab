1b51b2abe42da341a83e4f8bedf756f41b39f3df
jvuor <jussi.vuorinen@gmail.com>
2019-08-15 21:16:47 +0300

    add content to about and tutorial pages

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Anchor } from './Anchor';
 
 export const About = () => (
-  <p>
-    Yleistä tietoa
-  </p>
+  <React.Fragment>
+    <p>
+      Ohjelman kehitti <Anchor target="https://www.jussi.app">Jussi Vuorinen</Anchor>.
+    </p>
+    <p>
+      Lähdekoodi on julkaistu <Anchor target="https://github.com/jvuor/op-to-ynab">GitHubissa</Anchor>.
+    </p>
+  </React.Fragment>
 );
--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Anchor } from './Anchor';
 
 export const Tutorial = () => (
-  <p>
-    Ohjeet softan käyttämiseen
-  </p>
+  <div>
+    Näin saat oikeanlaisen CSV-tiedoston Osuuspankin sivuilta:
+    <div className="ml-8">
+      <ol className="list-decimal">
+        <li>Mene sivustolle <Anchor target="https://www.op.fi">www.op.fi</Anchor> työpöytäversioon.</li>
+        <li>Valitse Päivittäiset raha-asiat</li>
+        <li>Valitse Tapahtumavirta</li>
+        <li>Valitse ylhäältä tili jonka tapahtumat haluat siirtää.</li>
+        <li>
+          Tilivalintalistan oikealla puolella on nappula jossa on kolme pistettä.
+          Paina siitä ja valitse "Selaa tiliotteita".
+        </li>
+        <li>Valitse ajanjakso jolta haluat hakea tapahtumat ja paina sen valintanappia.</li>
+        <li>Avautuvasta Verkkotiliotteen katselu-ikkunasta valitse "Lataa pelkät tilitapahtumat tiedostona (csv)"</li>
+      </ol>
+    </div>
+  </div>
 );
