c70084ec08d219f18aa7e845246d742057903e8f
jvuor <jussi.vuorinen@gmail.com>
2019-08-15 21:16:29 +0300

    change sidenav layout to float to acommodate wider content

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Container = (props: any) => (
-  <div className="max-w-4xl m-2 mx-auto relative">
+  <div className="max-w-4xl m-2 pl-2 pr-2 mx-auto">
     {props.children}
   </div>
 );
--- src/components/SideNavContainer.tsx
+++ src/components/SideNavContainer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const SideNavContainer = (props: any) => (
-  <nav className="absolute right-0 w-48 border border-gray-500">
+  <nav className="float-right w-48 border border-gray-500">
     {props.children}
   </nav>
 );
